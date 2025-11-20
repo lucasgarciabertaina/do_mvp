2f0781045aa6ffe7aca0abed477444101da6319a
Lucas Garcia Bertaina <lucasgarciabertaina@gmail.com>
2025-11-20 17:21:03 -0300

    MVP WIP

--- app/api/push/test/route.ts
+++ app/api/push/test/route.ts
@@ -1,4 +1,4 @@
-import {Request, NextResponse } from 'next/server'
+import { Request, NextResponse } from 'next/server'
 import { pushToUser } from '@/lib/push'
 import { getCurrentUser } from '@/lib/auth'
 import prisma from '@/lib/prisma'
@@ -14,13 +14,13 @@ export async function POST(req: Request) {
     include: {
       owner: { select: { id: true, name: true, username: true, direction: true } }, // dueño del evento
       buyer: { select: { id: true, name: true, username: true, direction: true } }, // comprador asignado
-      }
+    }
   });
 
-  console.log("Current event:", doEvent)
+  console.log("Current event:", lastEvent)
   const res = await pushToUser(user.userId, {
     title: 'Para que no se olviden 🔔',
-    body: `Maniana es la penia en la casa de ${lastEvent?.owner?.username || 'desconocido'}! 🎉``,
+    body: `Maniana es la penia en la casa de ${lastEvent?.owner?.username || 'desconocido'}! 🎉`,
     data: { url: '/evento/actual' }
   })
   return NextResponse.json(res)
