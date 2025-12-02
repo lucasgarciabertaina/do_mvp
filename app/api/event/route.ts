c27022ab3b73031d0ad25c0ea1458fcf75713595
Lucas Garcia Bertaina <lucasgarciabertaina@gmail.com>
2025-12-02 16:26:13 -0300

    Fix bug

--- app/api/event/route.ts
+++ app/api/event/route.ts
@@ -1,6 +1,6 @@
 import {  NextResponse } from 'next/server'
 import {  getCurrentUser } from '@/lib/auth'
-import { prisma } from '@/lib/prisma'
+import prisma from '@/lib/prisma'
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
